77b18a869fccbaac158b70b32e76626b2cae9364
Silver Piir <silver.piir@vikk.ee>
2018-02-28 08:47:44 +0000

    update css files, add Doctrine

---
+++ README.md
@@ -0,0 +1,11 @@
+### Prerequisites
+
+Needed software in your computer to run this software:
+
+* php
+```
+sudo apt-get install php7.0-cli
+sudo apt-get install php-xml
+```
+
+* [composer](https://getcomposer.org/download/)
---
+++ web/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    // web/index.php
+    require_once __DIR__.'/../vendor/autoload.php';
+    
+    
+    $app = new Silex\Application();
+    
+    
+    // ... definitions
+    $app['debug'] = true;
+    
+    $app->register(new Silex\Provider\TwigServiceProvider(), [            
+        'twig.path' => __DIR__.'/../views',
+    ]);
+
+    $app->get('/bookings/create', function () use ($app) {
+        return $app['twig']->render('base.html.twig');
+    });
+
+    $app->run();
+
+
+
